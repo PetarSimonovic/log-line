5a7dde95787a8c8da943783dc431f380cec20776
Pete Simonovic <petar@simonovic.com>
2021-02-15 12:24:04 +0000

    basic text generator set up

--- spec/PromptSpec.js
+++ spec/PromptSpec.js
@@ -7,20 +7,35 @@ describe("Prompt", function() {
     prompt = new Prompt();
   });
 
-  it("should randomly pick a positive adjective from the posAdjBank array", function () {
+  it("should pick a positive adjective from the posAdjBank array", function () {
     expect(prompt.pickPosAdj()).toBe("happy");
   });
 
-  it("should randomly pick a negative adjective from the negAdjBank array", function () {
+  it("should pick a negative adjective from the negAdjBank array", function () {
     expect(prompt.pickNegAdj()).toBe("lazy");
   });
+
+
+  it("should pick a noun from the nounBank array", function () {
+    expect(prompt.pickNoun()).toBe("computer");
+  });
+
+  it("should pick a first-person verb from the verbBank array", function () {
+    expect(prompt.pickVerbFirstPerson()).toBe("rescue");
+  });
+
+  it("should pick a third-person verb from the verbBank array", function () {
+    expect(prompt.pickVerbThirdPerson()).toBe("destroys");
+  });
+
+
 
   // it("should assign a word from posAdjBank array to the posAdj_1 variable", function () {
   //   expect(prompt.assignPosAdj_1()).toBe("happy");
   // });
 
   it("should construct the log line", function () {
-    expect(prompt.constructLogLine()).toBe("A happy but lazy");
+    expect(prompt.constructLogLine()).toBe("A happy but lazy computer must rescue a happy computer before a lazy computer destroys a computer");
   });
 
 
--- src/Prompt.js
+++ src/Prompt.js
@@ -29,7 +29,35 @@ class Prompt {
     return negAdj
   };
 
+  pickNoun() {
+    let nounBank = ["computer"];
+    let choice = Math.floor(Math.random() * nounBank.length);
+    console.log(choice);
+    let noun = nounBank[choice];
+    console.log(noun)
+    return noun
+  };
+
+  pickVerbFirstPerson() {
+    let verbBank = ["rescue"];
+    let choice = Math.floor(Math.random() * verbBank.length);
+    console.log(choice);
+    let verb = verbBank[choice];
+    console.log(verb)
+    return verb
+  };
+
+  pickVerbThirdPerson() {
+    let verbBank = ["destroys"];
+    let choice = Math.floor(Math.random() * verbBank.length);
+    console.log(choice);
+    let verb = verbBank[choice];
+    console.log(verb)
+    return verb
+  };
+
+
   constructLogLine() {
-    return `A ${this.pickPosAdj()} but ${this.pickNegAdj()}`
+    return `A ${this.pickPosAdj()} but ${this.pickNegAdj()} ${this.pickNoun()} must ${this.pickVerbFirstPerson()} a ${this.pickPosAdj()} ${this.pickNoun()} before a ${this.pickNegAdj()} ${this.pickNoun()} ${this.pickVerbThirdPerson()} a ${this.pickNoun()}`
   };
 }
